557dfaa2667e39fe325b831822920548e74d8894
unknown <a.shataie2023@gmail.com>
2025-05-29 13:28:37 -0700

    feat: add basic Express server setup with routes and middlewares

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+
+const app = express();
+
+// * ----- Middlewares -----
+// ? Cors Policy
+app.use(cors());
+
+//? BodyParser
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+
+// * ----- Routes -----
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "main page" });
+});
+
+// * ----- 404 page -----
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    message: "page not found!",
+    success: false,
+  });
+});
+
+module.exports = app;
