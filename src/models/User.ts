a30acb079a1a2d72351723161889f363d9145c6e
unknown <a.shataie2023@gmail.com>
2025-05-30 09:53:32 -0700

    feat: create user model

---
+++ src/models/User.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+// create user schema
+const usersSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    bio: { type: String, required: false },
+    profilePic: { type: String, required: true }, // profile picture src
+    isVerified: { type: Boolean, default: false }, // for verify user
+    isPrivate: { type: String, default: false }, // private or public page
+  },
+  { timestamps: true }
+);
+
+// create user model
+export default mongoose.model("User", usersSchema);
