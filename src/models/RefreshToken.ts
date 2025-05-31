16c76f8efec8947e85a925cc386844ce10a54c36
unknown <a.shataie2023@gmail.com>
2025-05-31 08:22:15 -0700

    feat: add createToken static (A static from the refreshToken model)

--- src/models/RefreshToken.ts
+++ src/models/RefreshToken.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { v4 } from "uuid";
 const Schema = mongoose.Schema;
 
 // create refresh token schema
@@ -7,6 +8,28 @@ const refreshTokenSchema = new Schema({
   user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
   expireTime: { type: Date, required: true },
 });
+
+// create refresh token
+refreshTokenSchema.statics.createToken = async (user) => {
+  // set expire time for refresh token
+  const expire = process.env.REFRESH_TOKEN_EXPIRE_TIME
+    ? Number(process.env.REFRESH_TOKEN_EXPIRE_TIME)
+    : 30;
+
+  // create refresh token
+  const refreshToken = `${user._id}-${v4()}`;
+
+  // create and save new refresh token document in model
+  const tokenDoc = new model({
+    token: refreshToken,
+    user: user._id,
+    expireTime: new Date(Date.now() + expire * 24 * 3600 * 1000),
+  });
+
+  await tokenDoc.save();
+
+  return refreshToken;
+};
 
 // create refresh token model
 const model = mongoose.model("RefreshToken", refreshTokenSchema);
