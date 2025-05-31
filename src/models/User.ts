5428b5e4b9971b48f54207d139b0d48df09bbd7f
unknown <a.shataie2023@gmail.com>
2025-05-31 06:42:42 -0700

    feat: hash user password before save in user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 const Schema = mongoose.Schema;
 
 // create user schema
@@ -15,6 +16,20 @@ const usersSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// hash password before save in model
+usersSchema.pre("save", async function (next: any) {
+  // If the password has not been changed, it will next.
+  if (!this.isModified("password")) return next();
+
+  try {
+    // hash password
+    this.password = await bcrypt.hash(this.password, 10); 
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 // create user model
 export default mongoose.model("User", usersSchema);
