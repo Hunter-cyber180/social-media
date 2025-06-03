7ec809e88fdedb23f2110cb7f1268549680cd902
unknown <a.shataie2023@gmail.com>
2025-06-03 02:42:27 -0700

    feat: add auth route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 
+// ? ----- Controllers -----
+import authController from "./auth/authController";
+
 const app = express();
 
 // * ----- Middlewares -----
@@ -15,6 +18,8 @@ app.use(express.json({ limit: "50mb" }));
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "main page" });
 });
+
+app.use("/auth", authController);
 
 // * ----- 404 page -----
 app.use((req: Request, res: Response, next: NextFunction) => {
