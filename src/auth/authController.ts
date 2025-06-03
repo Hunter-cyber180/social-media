ba1ad8d166a5d247b6536183712dcfb0b32c38e2
unknown <a.shataie2023@gmail.com>
2025-06-03 01:56:58 -0700

    feat: add register user controller

--- src/auth/authController.ts
+++ src/auth/authController.ts
@@ -1,5 +1,27 @@
-import { Router } from "express";
+import { Request, Response, NextFunction, Router } from "express";
+import ValidationMiddleware from "../middlewares/validate";
+import CreateUserDto from "./dto/createUserDto";
+import { UserRegister } from "./dto/userDto";
+import { register } from "./authServices";
 
 const router = Router();
+
+// register user
+router.post(
+  "/register",
+  ValidationMiddleware(CreateUserDto),
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    // get data from req.body and register user
+    const body: UserRegister = req.body;
+    const data = await register(body);
+
+    // return json response
+    res.status(201).json({
+      message: "User Registered successfully!",
+      data,
+      success: true,
+    });
+  }
+);
 
 export default router;
