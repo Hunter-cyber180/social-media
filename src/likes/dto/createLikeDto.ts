4c760ab47db481b76d3945562c526759bddfdf1a
unknown <a.shataie2023@gmail.com>
2025-06-04 13:20:45 -0700

    feat: add post ID validation to CreateLikeDto

--- src/likes/dto/createLikeDto.ts
+++ src/likes/dto/createLikeDto.ts
@@ -7,6 +7,12 @@ class CreateLikeDto {
   @IsString({ message: "User Id type must be a string!" })
   @IsMongoId({ message: "User Id is not valid!" })
   user: Types.ObjectId;
+
+  // post ID validation
+  @IsNotEmpty({ message: "Post Id can not be empty!" })
+  @IsString({ message: "Post Id type must be a string!" })
+  @IsMongoId({ message: "Post Id is not valid!" })
+  post: Types.ObjectId;
 }
 
 export default CreateLikeDto;
