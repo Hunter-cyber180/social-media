047c3f30eb9f92d7ad470c0a4500ba52e57bb84b
unknown <a.shataie2023@gmail.com>
2025-06-05 03:42:53 -0700

    feat: add post ID validation to CreateReplyCommentDto

--- src/replyComments/dto/createReplyCommentDto.ts
+++ src/replyComments/dto/createReplyCommentDto.ts
@@ -8,6 +8,12 @@ class CreateReplyCommentDto {
   @IsString({ message: "User Id type must be a string!" })
   @IsMongoId({ message: "User Id is not valid!" })
   user: Types.ObjectId;
+
+  // post ID validation
+  @IsNotEmpty({ message: "Post Id can not be empty!" })
+  @IsString({ message: "Post Id type must be a string!" })
+  @IsMongoId({ message: "Post Id is not valid!" })
+  post: Types.ObjectId;
 }
 
 export default CreateReplyCommentDto;
