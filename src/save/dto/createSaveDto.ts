02ee8cea188cde106f1ac9e141360e40d41e53a6
unknown <a.shataie2023@gmail.com>
2025-06-05 03:33:39 -0700

    feat: add CreateSaveDto

---
+++ src/save/dto/createSaveDto.ts
@@ -0,0 +1,13 @@
+import { IsMongoId, IsNotEmpty, IsString } from "class-validator";
+import { Types } from "mongoose";
+
+// create save DTO
+class CreateSaveDto {
+  // user ID validation
+  @IsNotEmpty({ message: "User Id can not be empty!" })
+  @IsString({ message: "User Id type must be a string!" })
+  @IsMongoId({ message: "User Id is not valid!" })
+  user: Types.ObjectId;
+}
+
+export default CreateSaveDto;
