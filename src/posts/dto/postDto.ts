3f62afe509ce02b508b0763e437cc941c329eebe
unknown <a.shataie2023@gmail.com>
2025-06-06 16:31:16 -0700

    feat: create post interface

--- src/posts/dto/postDto.ts
+++ src/posts/dto/postDto.ts
@@ -1,5 +1,15 @@
+import { Types } from "mongoose";
+
 // media interface
 export interface IMedia {
   path: string;
   filename: string;
+}
+
+// post interface
+export default interface Post extends Document {
+  media: IMedia;
+  desc: string;
+  tags?: string[];
+  user: Types.ObjectId;
 }
