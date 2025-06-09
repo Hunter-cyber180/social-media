36f05be20bd1afee58bc295920880eeee109d0df
unknown <a.shataie2023@gmail.com>
2025-06-08 22:09:21 -0700

    feat: create deletePost service

--- src/posts/postServices.ts
+++ src/posts/postServices.ts
@@ -1,10 +1,13 @@
 import ClientError from "../errors/clientError";
 import PostModel from "../models/Post";
 import UserModel from "../models/User";
+import LikeModel from "../models/Like";
+import SaveModel from "../models/Save";
+import CommentModel from "../models/Comment";
 
 // create post service
 export const addPost = async (desc: string, tags: string[], userID: string) => {
-    // get user by userID
+  // get user by userID
   const user = await UserModel.findOne({ _id: userID });
   if (!user) throw new ClientError("User Not Found!", 404);
 
@@ -24,4 +27,27 @@ export const addPost = async (desc: string, tags: string[], userID: string) => {
   await post.save();
 
   return post;
+};
+
+// delete post service
+export const deletePost = async (userID: string, postID: string) => {
+  // get user
+  const user = await UserModel.findOne({ _id: userID });
+  if (!user) throw new ClientError("User not found!", 404);
+
+  // get post
+  const post = await PostModel.findOne({ _id: postID });
+  if (!post || post.user.toString() !== user._id.toString())
+    throw new ClientError("Cant remove this post!", 400);
+
+  // TODO => delete post media
+
+  // Delete likes, comments, saved posts, and post
+  await LikeModel.deleteMany({ post: postID });
+  await SaveModel.deleteMany({ post: postID });
+  await CommentModel.deleteMany({ post: postID });
+
+  await PostModel.findByIdAndDelete(postID);
+
+  return;
 };
