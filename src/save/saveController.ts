dba53ec18093d30ddf90be341fafc9feaabea9d3
unknown <a.shataie2023@gmail.com>
2025-06-10 00:28:05 -0700

    feat: Create save controller

---
+++ src/save/saveController.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Router, Request, Response } from "express";
+import { save } from "./saveServices";
+
+const router = Router();
+
+// save controller
+router.post(
+  "/:userID/:postID",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      // get userID and postID from req.params and save post
+      const { userID, postID } = req.params;
+      await save(userID, postID);
+
+      // return json response
+      res.status(201).json({
+        message: "Save Created Successfully!",
+        success: true,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+export default router;
