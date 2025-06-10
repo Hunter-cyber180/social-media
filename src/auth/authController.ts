b3deae5a6c529e495b5a473080ea61bd021d5cb5
unknown <a.shataie2023@gmail.com>
2025-06-10 00:07:24 -0700

    refactor: Add Try Catch to auth controller

--- src/auth/authController.ts
+++ src/auth/authController.ts
@@ -24,16 +24,20 @@ router.post(
   "/register",
   ValidationMiddleware(RegisterUserDto),
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    // get data from req.body and register user
-    const body: UserRegister = req.body;
-    const data = await register(body);
+    try {
+      // get data from req.body and register user
+      const body: UserRegister = req.body;
+      const data = await register(body);
 
-    // return json response
-    res.status(201).json({
-      message: "User Registered successfully!",
-      data,
-      success: true,
-    });
+      // return json response
+      res.status(201).json({
+        message: "User Registered successfully!",
+        data,
+        success: true,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
@@ -42,16 +46,20 @@ router.post(
   "/login",
   ValidationMiddleware(LoginUserDto),
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    // get data from req.body and login user
-    const body: User = req.body;
-    const data = await login(body);
+    try {
+      // get data from req.body and login user
+      const body: User = req.body;
+      const data = await login(body);
 
-    // return json response
-    res.status(200).json({
-      message: "User Logined successfully!",
-      data,
-      success: true,
-    });
+      // return json response
+      res.status(200).json({
+        message: "User Logined successfully!",
+        data,
+        success: true,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
@@ -59,16 +67,20 @@ router.post(
 router.get(
   "/refresh/:token",
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    // get data from req.params and create new access token
-    const { token } = req.params;
-    const data = await refreshToken(token);
+    try {
+      // get data from req.params and create new access token
+      const { token } = req.params;
+      const data = await refreshToken(token);
 
-    // return json response
-    res.status(200).json({
-      message: "Create Refresh Token was successfully!",
-      data,
-      success: true,
-    });
+      // return json response
+      res.status(200).json({
+        message: "Create Refresh Token was successfully!",
+        data,
+        success: true,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
@@ -77,16 +89,20 @@ router.post(
   "/forget-password",
   ValidationMiddleware(ForgetPasswordDto),
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    // get data from req.body and sent reset password email
-    const { email } = req.body;
-    const { token } = await forgetPassword(email);
+    try {
+      // get data from req.body and sent reset password email
+      const { email } = req.body;
+      const { token } = await forgetPassword(email);
 
-    // return json response
-    res.status(200).json({
-      message: "Password reset email sent.",
-      data: { token },
-      success: true,
-    });
+      // return json response
+      res.status(200).json({
+        message: "Password reset email sent.",
+        data: { token },
+        success: true,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
@@ -95,15 +111,19 @@ router.post(
   "/reset-password",
   ValidationMiddleware(ResetPasswordDto),
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    // get data from req.body and set new password for user
-    const { token, password } = req.body;
-    await resetPassword(token, password);
+    try {
+      // get data from req.body and set new password for user
+      const { token, password } = req.body;
+      await resetPassword(token, password);
 
-    // return json response
-    res.status(200).json({
-      message: "Set New Password was successfully!",
-      success: true,
-    });
+      // return json response
+      res.status(200).json({
+        message: "Set New Password was successfully!",
+        success: true,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
