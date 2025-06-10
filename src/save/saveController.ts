a6cc0751488c7a5b8b9212ab2d3db3869ab97b8a
unknown <a.shataie2023@gmail.com>
2025-06-10 00:46:35 -0700

    feat: Create unsave controller

--- src/save/saveController.ts
+++ src/save/saveController.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Router, Request, Response } from "express";
-import { save } from "./saveServices";
+import { save, unsave } from "./saveServices";
 
 const router = Router();
 
@@ -15,6 +15,26 @@ router.post(
       // return json response
       res.status(201).json({
         message: "Save Created Successfully!",
+        success: true,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// unsave controller
+router.post(
+  "/:userID/:postID",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      // get userID and postID from req.params and unsave post
+      const { userID, postID } = req.params;
+      await unsave(userID, postID);
+
+      // return json response
+      res.status(200).json({
+        message: "Save Deleted Successfully!",
         success: true,
       });
     } catch (error) {
