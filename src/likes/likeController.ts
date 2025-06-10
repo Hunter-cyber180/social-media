2df44a72819135f16348813ceb3708c3abe624a2
unknown <a.shataie2023@gmail.com>
2025-06-09 23:53:53 -0700

    feat: Create unlike post controller

--- src/likes/likeController.ts
+++ src/likes/likeController.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Router, Request, Response } from "express";
-import { like } from "./likeServices";
+import { like, unlike } from "./likeServices";
 
 const router = Router();
 
@@ -14,6 +14,22 @@ router.post(
     // return json response
     res.status(200).json({
       message: "Post Liked Successfully!",
+      success: true,
+    });
+  }
+);
+
+// unlike post controller
+router.post(
+  "/:userID/:postID",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    // get postID and userID from req.params and like post
+    const { postID, userID } = req.params;
+    await unlike(userID, postID);
+
+    // return json response
+    res.status(200).json({
+      message: "Post Unliked Successfully!",
       success: true,
     });
   }
