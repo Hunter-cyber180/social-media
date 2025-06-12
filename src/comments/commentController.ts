48a9e65c83171c7d589cf65c97075398102054f8
unknown <a.shataie2023@gmail.com>
2025-06-11 23:24:21 -0700

    feat: Add create comment controller

--- src/comments/commentController.ts
+++ src/comments/commentController.ts
@@ -1,5 +1,35 @@
-import { Router } from "express";
+import { NextFunction, Router, Request, Response } from "express";
+
+// * ----- Middlewares -----
+import ValidationMiddleware from "../middlewares/validate";
+
+// * ----- DTO -----
+import CreateCommentDto from "./dto/createCommentDto";
+import Comment from "./dto/commentDto";
+import { create } from "./commentServices";
 
 const router = Router();
+
+// add comment controller
+router.post(
+  "/",
+  ValidationMiddleware(CreateCommentDto),
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      // get body from req.body and create new comment
+      const body: Comment = req.body;
+      const comment = await create(body);
+
+      // return json response
+      res.status(201).json({
+        message: "Comment Added Successfully!",
+        data: comment,
+        success: true,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default router;
