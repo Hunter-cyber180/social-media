21348fe7e8ce1e38f4b6f0f167015365094c4e6a
unknown <a.shataie2023@gmail.com>
2025-06-11 23:41:52 -0700

    docs: Add comments for follow service

--- src/follow/followServices.ts
+++ src/follow/followServices.ts
@@ -4,24 +4,28 @@ import ClientError from "../errors/clientError";
 import UserModel from "../models/User";
 import FollowModel from "../models/Follow";
 
+// follow page service
 export const follow = async (userID: string, pageID: string) => {
+  // get follower and check existing follower
   const user = await UserModel.findOne({ _id: userID });
-  if(!user)
-    throw new ClientError("User not found!", 404);
+  if (!user) throw new ClientError("User not found!", 404);
 
+  // get page and check existing page
   const page = await UserModel.findOne({ _id: pageID });
   if (!page) throw new ClientError("Page not found to follow!", 404);
 
+  // check if follower and following are the same
   if (user._id.toString() === pageID)
     throw new ClientError("You cannot follow yourself!", 400);
 
+  // check existing follow
   const existingFollow = await FollowModel.findOne({
     follower: user._id,
     following: pageID,
   });
-
   if (existingFollow) throw new ClientError("Page already followed!", 400);
 
+  // create follow
   const follow = await FollowModel.create({
     follower: user._id,
     following: pageID,
