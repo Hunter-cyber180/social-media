de2fa28d7b8a2bdcaacd0714fc63d005bb84b354
unknown <a.shataie2023@gmail.com>
2025-06-12 23:38:11 -0700

    docs: Add comments for create reply comment controller

--- src/replyComments/replyCommentController.ts
+++ src/replyComments/replyCommentController.ts
@@ -10,17 +10,19 @@ import { create } from "./replyCommentServices";
 import ReplyComment from "./dto/replyCommentDto";
 import CreateReplyCommentDto from "./dto/createReplyCommentDto";
 
-
 const router = Router();
 
+// create reply comment controller
 router.post(
   "/",
   ValidationMiddleware(CreateReplyCommentDto),
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
+      // get data from req.body and create reply comment
       const body: ReplyComment = req.body;
       const comment = await create(body);
 
+      // return json response
       res.status(201).json({
         message: "Reply Comment Added Successfully!",
         data: comment,
