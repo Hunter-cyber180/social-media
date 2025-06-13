5ba66477c9bbf1074c666e6d630f0005cd3e3003
unknown <a.shataie2023@gmail.com>
2025-06-12 23:31:29 -0700

    feat: Add create reply comment controller

--- src/replyComments/replyCommentController.ts
+++ src/replyComments/replyCommentController.ts
@@ -1,5 +1,35 @@
-import { Router } from "express";
+import { Router, Request, Response, NextFunction } from "express";
+
+// * ----- Middlewares -----
+import ValidationMiddleware from "../middlewares/validate";
+
+// * ----- Services -----
+import { create } from "./replyCommentServices";
+
+// * ----- DTO -----
+import ReplyComment from "./dto/replyCommentDto";
+import CreateReplyCommentDto from "./dto/createReplyCommentDto";
+
 
 const router = Router();
+
+router.post(
+  "/",
+  ValidationMiddleware(CreateReplyCommentDto),
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const body: ReplyComment = req.body;
+      const comment = await create(body);
+
+      res.status(201).json({
+        message: "Reply Comment Added Successfully!",
+        data: comment,
+        success: true,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default router;
