3306e08e94facb4a27870dd0a48cf705c9bb8ab2
unknown <a.shataie2023@gmail.com>
2025-06-15 23:49:58 -0700

    docs: Add comments for get user page controller

--- src/page/pageController.ts
+++ src/page/pageController.ts
@@ -1,15 +1,20 @@
 import { Router, Request, Response, NextFunction } from "express";
+
+// * ----- services -----
 import { getUserPage } from "./pageServices";
 
 const router = Router();
 
+// get user page controller
 router.get(
   "/:userID/:pageID",
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
+      // get userID and pageID from req.params and get user page data
       const { userID, pageID } = req.params;
       const data = getUserPage(userID, pageID);
 
+      // return json response
       res.status(200).json({
         message: "Get User Page Was Successfully!",
         data,
