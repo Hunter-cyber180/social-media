15b3dd7c559be8186a1c5836205b3abceb0194fe
unknown <a.shataie2023@gmail.com>
2025-06-15 23:47:23 -0700

    feat: Create get user page controller

--- src/page/pageController.ts
+++ src/page/pageController.ts
@@ -1,5 +1,24 @@
 import { Router, Request, Response, NextFunction } from "express";
+import { getUserPage } from "./pageServices";
 
 const router = Router();
+
+router.get(
+  "/:userID/:pageID",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const { userID, pageID } = req.params;
+      const data = getUserPage(userID, pageID);
+
+      res.status(200).json({
+        message: "Get User Page Was Successfully!",
+        data,
+        success: true,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default router;
