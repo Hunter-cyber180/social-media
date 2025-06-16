5d4c4d03f10f5b2b597bd25bda184010eb9a3f97
unknown <a.shataie2023@gmail.com>
2025-06-15 23:05:56 -0700

    docs: Add comments for update user profile controller

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,14 +1,19 @@
 import { Router, Request, Response, NextFunction } from "express";
+
+// * ----- Services -----
 import { profile } from "./userServices";
 
 const router = Router();
 
+// update user profile controller
 router.put(
   "/:userID",
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    // get userID from req.params and update user profile
     const { userID } = req.params;
     await profile(userID, req.file);
 
+    // return json response
     res.status(200).json({
       message: "Profile Updated Successfully!",
       success: true,
