02c999dd0c1b1b85c112ccd1edef17e1a7ffc0e0
unknown <a.shataie2023@gmail.com>
2025-06-15 23:03:16 -0700

    feat: Create update profile controller

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,5 +1,19 @@
-import { Router } from "express";
+import { Router, Request, Response, NextFunction } from "express";
+import { profile } from "./userServices";
 
 const router = Router();
+
+router.put(
+  "/:userID",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const { userID } = req.params;
+    await profile(userID, req.file);
+
+    res.status(200).json({
+      message: "Profile Updated Successfully!",
+      success: true,
+    });
+  }
+);
 
 export default router;
