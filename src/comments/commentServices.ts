53da2c9ed21837eb3c26d9ebce4921994cadac05
unknown <a.shataie2023@gmail.com>
2025-06-25 22:52:19 -0700

    refactor: throw new clientError instead of new Error in post comment service

--- src/comments/commentServices.ts
+++ src/comments/commentServices.ts
@@ -6,17 +6,19 @@ import CommentModel from "../models/Comment";
 import PostModel from "../models/Post";
 import UserModel from "../models/User";
 
+import ClientError from "../errors/clientError";
+
 // add comment
 export const create = async (body: Comment) => {
   const { post: postID, user: userID, content } = body;
 
   // Checking the existence of the user
   const user = await UserModel.findOne({ _id: userID });
-  if (!user) throw new Error("User not found!");
+  if (!user) throw new ClientError("User not found!", 404);
 
   // Checking the existence of the post
   const post = await PostModel.findOne({ _id: postID });
-  if (!post) throw new Error("Post not found!");
+  if (!post) throw new ClientError("Post not found!", 404);
 
   // create comment
   const comment = new CommentModel({ post: post._id, user: user._id, content });
